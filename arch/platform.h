63935ad0f5343ab581675df11d86666b86688628
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-09-01 14:45:54 +0100

    reset: remove legacy OS

--- arch/platform.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdint.h>
-namespace pilab::platform {
-enum class Arch { Armv6, Armv7, Arm64, X86, X86_64 };
-struct Info { Arch arch; uintptr_t peripheral_base; uintptr_t uart_base; };
-const Info& info();
-void early_uart_init();
-void early_putc(char c);
-}
