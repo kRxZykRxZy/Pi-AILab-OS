16a6ae0ea745e1d0ce1675cbd6ef4ae9c839a3b6
kRxZy <192328467+kRxZykRxZy@users.noreply.github.com>
2026-09-01 11:47:55 +0100

    usb: expand native device descriptor

--- drivers/usb/usb.h
+++ drivers/usb/usb.h
@@ -1,3 +1,6 @@
 #pragma once
 #include <stdint.h>
-namespace pilab::usb { struct Device{uint16_t vid,pid;bool connected;}; void init(); uint32_t count(); const Device* devices(); }
+namespace pilab::usb {
+struct Device { uint8_t address; uint16_t vid,pid; uint8_t class_code; bool connected; };
+void init(); uint32_t count(); const Device* devices();
+}
